20636207ad0b1bacbf00cc9db996160c2d4019b6
Edukons <eduardo.kons03@gmail.com>
2024-07-16 10:31:06 -0300

    category service

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.g3b27l4pr9"
+    "revision": "0.el5vbc2edv"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
